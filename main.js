f3fb93eb9a7cdb4545e5ed5ba2edf4f687fff568
mark <markpackham1@gmail.com>
2022-01-13 15:40:56 +0000

    create a render instance
    https://youtu.be/zOZ8Ah26QOc?t=810

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 function main() {
-  // scene camera & render, 3 essentials
+  // scene, camera & render, 3 essentials
 
-  const canvas = document.querySelector('#c');
+  const canvas = document.querySelector("#c");
 
   // field of view
   const fov = 50;
   // aspect is how many times width is bigger than height
-  const aspect = 1;
+  const aspect = canvas.clientWidth / canvas.clientHeight;
   // the clipping plane, so ignore objects less than a distance towards us, this is the default value
-  // const near = 0.1;
-  const near = canvas.clientWidth / canvas.clientHeight;
+  const near = 0.1;
   // the opposite clipping plane to the above, this is the default value
   const far = 2000;
 
-  new THREE.PerspectiveCamera(fov, aspect, near, far){
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-  }
+  const renderer = new THREE.WebGLRenderer({ canvas });
 }
 
 main();
