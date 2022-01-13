1716d550cd7f8ff62246272236ef9359a99066f8
mark <markpackham1@gmail.com>
2022-01-13 17:17:27 +0000

    change field of view for sharper image
    https://youtu.be/zOZ8Ah26QOc?t=1323

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function main() {
   const canvas = document.querySelector("#c");
 
   // field of view
-  const fov = 50;
+  const fov = 75;
   // aspect is how many times width is bigger than height
   const aspect = canvas.clientWidth / canvas.clientHeight;
   // the clipping plane, so ignore objects less than a distance towards us, this is the default value
