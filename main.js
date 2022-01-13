11b57514a16513d7f0a67cbb238b4c0d6cb56cb7
mark <markpackham1@gmail.com>
2022-01-13 15:34:04 +0000

    add scene, camera & render variables
    https://youtu.be/zOZ8Ah26QOc?t=665

---
+++ main.js
@@ -0,0 +1,21 @@
+function main() {
+  // scene camera & render, 3 essentials
+
+  const canvas = document.querySelector('#c');
+
+  // field of view
+  const fov = 50;
+  // aspect is how many times width is bigger than height
+  const aspect = 1;
+  // the clipping plane, so ignore objects less than a distance towards us, this is the default value
+  // const near = 0.1;
+  const near = canvas.clientWidth / canvas.clientHeight;
+  // the opposite clipping plane to the above, this is the default value
+  const far = 2000;
+
+  new THREE.PerspectiveCamera(fov, aspect, near, far){
+
+  }
+}
+
+main();
